bc3de8eb80b55371c667daa2d0c3159e044e7135
Christine Tran <christinetran825@gmail.com>
2017-10-26 02:01:52 +0000

    updated users table to include email and password digest column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,24 +7,30 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # get "/" do
-  #   erb :index
-  # end
-  #
-  # get "/signup" do
-  #   erb :'/signup'
-  # end
-  #
-  # post "/signup" do
-  #
-  # end
-  #
-  # get "/login" do
-  #   erb :'/login'
-  # end
-  #
-  # get "/logout" do
-  #   erb :'/logout'
-  # end
+  get '/' do
+    erb :"/index"
+  end
+
+  get "/signup" do
+    erb :"/signup"
+  end
+
+  post '/signup' do
+    @user = User.create(params)
+    binding.pry
+    redirect :"/users/account"
+  end
+
+  get '/users/account' do
+    erb :"/users/account"
+  end
+
+  get "/login" do
+    erb :'/login'
+  end
+
+  get '/logout' do
+    erb :'/logout'
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025220400) do
+ActiveRecord::Schema.define(version: 20171026015651) do
 
   create_table "tea_types", force: :cascade do |t|
     t.integer "tea_id"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20171025220400) do
   create_table "users", force: :cascade do |t|
     t.string  "name"
     t.integer "tea_id"
+    t.string  "email"
+    t.string  "password_digest"
   end
 
 end
