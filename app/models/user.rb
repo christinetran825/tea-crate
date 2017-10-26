88767f3b01f46f27c7e1503516e2f9fc42554c1e
Christine Tran <christinetran825@gmail.com>
2017-10-26 22:03:57 +0000

    update self.find_by method to self.find_by_slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
     name.downcase.gsub(" ","-")
   end
 
-  def self.find_by(slug)
-    User.all.find_by {|user| user.slug == slug}
+  def self.find_by_slug(slug)
+    User.all.find {|user| user.slug == slug}
   end
 end
