e255d7122df2f721e6bbb1ac3bfcec485e053063
Christine Tran <christinetran825@gmail.com>
2017-10-26 04:56:31 +0000

    added slug methods to models/user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_many :teas
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by(slug)
+    User.all.find_by {|user| user.slug == slug}
+  end
 end
