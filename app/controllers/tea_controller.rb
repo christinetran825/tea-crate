c6541282a70b14431f7ea1597a25a907dc6ffa59
Christine Tran <christinetran825@gmail.com>
2017-10-26 17:42:59 +0000

    added and updated controller routes for TeaController

--- app/controllers/tea_controller.rb
+++ app/controllers/tea_controller.rb
@@ -2,19 +2,46 @@ class TeaController < ApplicationController
 
   get '/teas' do
     if !logged_in
-      redirect '/login'
+      redirect "/login"
     else
+      @teas = Tea.all
       erb :'/teas/index'
     end
   end
 
   get '/teas/new' do
     if !logged_in
-      redirect '/login'
+      redirect "/login"
     else
       erb :'/teas/create_tea'
     end
   end
 
+  post '/teas' do
+    if params.has_any?("")
+      redirect "/teas/new"
+    else
+      # @tea = Tea.create
+      erb :'/teas/:id'
+    end
+  end
+
+  get '/teas/:id' do
+    @tea = Tea.find(params[:id])
+    erb :'/teas/show_tea'
+  end
+
+  get '/teas/:id/edit' do
+
+    erb :'/teas/edit_tea'
+  end
+
+  patch '/teas/:id' do
+    @tea = Tea.find(params[:id])
+  end
+
+  delete '/teas/:id' do
+    Tea.destroy(params[:tea])
+  end
 
 end
