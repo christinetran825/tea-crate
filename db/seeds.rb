31fb4ed1bfe44b10680329d5b0d2c7e831d93e40
Christine Tran <christinetran825@gmail.com>
2017-10-31 22:06:25 +0000

    added create methods for Types

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Type.create(type_name: "Black")
+Type.create(origin: "China")
+Type.create(leaves: "loose leaf")
+Type.create(caffeine: "caffeine")
+Type.create(pairing: "none")
+Type.create(brew_time: "3 minutes")
