1f80b81b08bf497ccef1fbf0836cbb98a19c5a3e
Christine Tran <christinetran825@gmail.com>
2017-10-31 22:04:14 +0000

    removed other has_many associations and methods

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,24 +1,4 @@
 class Tea < ActiveRecord::Base
   belongs_to :user
   has_many :types
-  has_many :origins, through: :types
-  has_many :leaves, through: :types
-  has_many :caffeine, through: :types
-  has_many :pairings, through: :types
-  has_many :brew_times, through: :types
-  has_many :brands, through: :types
-
-
-  attr_reader :name
-
-  TEA = []
-
-  def initialize(params)
-    @tea_name = params[:tea_name]
-    TEA << self
-  end
-
-  def self.all
-    TEA
-  end
 end
