607813ab08f080b330fee3ec6c1d0aed3103ca5c
Christine Tran <christinetran825@gmail.com>
2017-10-31 01:22:43 +0000

    updated has_many relationships

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,9 +1,10 @@
 class Tea < ActiveRecord::Base
   belongs_to :user
   has_many :types
-  has_many :origins
+  has_many :origins, through: :types
+  has_many :leaves, through: :types
+  has_many :caffeine, through: :types
   has_many :pairings, through: :types
   has_many :brew_times, through: :types
   has_many :brands, through: :types
 end
- 
