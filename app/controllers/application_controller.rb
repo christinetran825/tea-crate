788f31de2649632551c820be14154e10a07b8dc8
Christine Tran <christinetran825@gmail.com>
2017-11-01 23:43:33 +0000

    moved signup, login, and user routes to User Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,61 +13,6 @@ class ApplicationController < Sinatra::Base
     erb :'/index'
   end
 
-  get '/users/signup' do
-    if !logged_in
-      erb :'/registrations/signup'
-    else
-      # redirect "/teas"
-      redirect "/users/#{@user.id}/teas"
-    end
-  end
-
-  post '/signup' do
-    if params.has_value?("")
-      redirect "/users/signup"
-    else
-      @user = User.new
-      @user.username = params[:username]
-      @user.email = params[:email]
-      @user.password = params[:password]
-      @user.save
-      session[:user_id] = @user.id
-      redirect "/users/#{@user.id}"
-    end
-  end
-
-  get '/login' do
-    if !logged_in
-      erb :'/login'
-    else
-      redirect "/users/#{@user.id}/teas"
-    end
-  end
-
-  post '/login' do
-    @user = User.find_by_slug(params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect "/users/#{@user.id}"
-    else
-      redirect "/login"
-    end
-  end
-
-  get '/logout' do
-    if !logged_in
-      redirect "/"
-    else
-      session.clear
-      redirect "/login"
-    end
-  end
-
-  get '/users/:id' do
-    @user = User.find_by(session[:user_id])
-    erb :'/users/show'
-  end
-
   get '/teas/tea-info/history-tea' do
     erb :'/tea-info/history_tea'
   end
