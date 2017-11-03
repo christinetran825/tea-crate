257cadcc0588d54cf2146c76de612c22d1b151b5
Christine Tran <christinetran825@gmail.com>
2017-11-03 19:26:16 +0000

    updated routes to determine tea ids

--- app/controllers/tea_controller.rb
+++ app/controllers/tea_controller.rb
@@ -3,6 +3,8 @@ class TeaController < ApplicationController
   get '/users/:id/teas' do
     @user = current_user
     @teas = Tea.all
+    @types = Type.all
+    # binding.pry
     erb :'/teas/index'
   end
 
@@ -16,39 +18,34 @@ class TeaController < ApplicationController
       redirect "/users/#{@user.id}/teas/new"
     else
       @user = current_user
+      # binding.pry
       @tea = Tea.create
       @tea.tea_name = params[:tea][:tea_name]
       @tea.types << Type.create(params[:tea][:type])
       @tea.save
-      # binding.pry
       redirect "/users/#{@user.id}/teas/#{@tea.id}"
-      # @user = current_user
-      # @tea = Tea.create
-      # @tea.tea_name = params[:tea][:tea_name]
-      # @tea.save
-      # @tea.types << Type.create(params[:tea][:type])
-      # redirect "/users/#{@user.id}/teas/#{@tea.id}"
     end
   end
 
   get '/users/:id/teas/:tea_id' do
     @user = current_user
+    # binding.pry
     @tea = Tea.find_by(params[:id])
     @type = Type.find_by(params[:id])
-    # @new_tea = TeaType.create(params)
-    # binding.pry
     erb :'/teas/show_tea'
   end
 
   get '/users/:id/teas/:tea_id/edit' do
     @user = current_user
     @tea = Tea.find_by(params[:id])
+    @type = Type.find_by(params[:id])
     erb :'/teas/edit_tea'
   end
 
   post '/users/:id/teas/:tea_id' do
     @user = current_user
     @tea = Tea.find_by(params[:id])
+    @type = Type.find_by(params[:id])
     @tea.types.update(params[:tea][:type])
     @tea.save
     redirect "/users/#{@user.id}/teas/#{@tea.id}"
