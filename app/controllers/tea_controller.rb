dbaa2f6b92b62ca3215fa4ff7d830b022751b7cc
Christine Tran <christinetran825@gmail.com>
2017-11-04 08:45:10 +0000

    updated Tea.find(params[:id]) to params[:tea_id)

--- app/controllers/tea_controller.rb
+++ app/controllers/tea_controller.rb
@@ -18,7 +18,6 @@ class TeaController < ApplicationController
       redirect "/users/#{@user.id}/teas/new"
     else
       @user = current_user
-      binding.pry
       @tea = Tea.create
       @tea.tea_name = params[:tea][:tea_name]
       @tea.types << Type.create(params[:tea][:type])
@@ -30,22 +29,23 @@ class TeaController < ApplicationController
   get '/users/:id/teas/:tea_id' do
     @user = current_user
     # binding.pry
-    @tea = Tea.find_by(params[:id])
-    @type = Type.find_by(params[:id])
+    @tea = Tea.find(params[:tea_id])
+    @type = Type.find(params[:tea_id])
     erb :'/teas/show_tea'
   end
 
   get '/users/:id/teas/:tea_id/edit' do
     @user = current_user
-    @tea = Tea.find_by(params[:id])
-    @type = Type.find_by(params[:id])
+    binding.pry
+    @tea = Tea.find(params[:tea_id])
+    @type = Type.find(params[:tea_id])
     erb :'/teas/edit_tea'
   end
 
   post '/users/:id/teas/:tea_id' do
     @user = current_user
-    @tea = Tea.find_by(params[:id])
-    @type = Type.find_by(params[:id])
+    @tea = Tea.find(params[:tea_id])
+    @type = Type.find(params[:tea_id])
     @tea.types.update(params[:tea][:type])
     @tea.save
     redirect "/users/#{@user.id}/teas/#{@tea.id}"
@@ -53,7 +53,7 @@ class TeaController < ApplicationController
 
   delete '/users/:id/teas/:tea_id' do
     @user = current_user
-    @tea = Tea.find_by(params[:id])
+    @tea = Tea.find(params[:tea_id])
     @tea.destroy
     redirect "/users/:id/teas"
   end
