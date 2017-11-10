de5cacff9dc1622a92addfbdcbc425525e121943
Christine Tran <christinetran825@gmail.com>
2017-11-10 04:37:29 +0000

    updated all routes

--- app/controllers/tea_controller.rb
+++ app/controllers/tea_controller.rb
@@ -1,57 +1,58 @@
 class TeaController < ApplicationController
 
   get '/users/:slug/teas' do
-    if !logged_in?
-      redirect '/login'
-    else
+    if logged_in?
       @user = current_user
-      # session[:user_id] = @user.id
-      @teas = @user.teas
-      # binding.pry
       erb :'/teas/index'
+    else
+      redirect '/login'
     end
   end
 
   get '/users/:slug/teas/new' do
-    if !logged_in?
-      redirect '/login'
-    else
+    if logged_in?
       @user = current_user
       @types = Type.all
       erb :'/teas/create_tea'
+    else
+      redirect '/login'
     end
   end
 
   post '/users/:slug/teas' do
-    if params.has_key?("")
+    if params.values.any? {|value| value == ""}
       flash[:message] = "Please enter all fields."
       redirect "/users/#{@user.slug}/teas/new"
     else
-      @user = current_user
+      user = current_user
       @tea = Tea.create
       @tea.tea_name = params[:tea][:tea_name]
       @tea.user_id = user.id
-      @tea.types << Type.create(params[:tea][:type])
+      # binding.pry
+      @tea.save
+      @type = Type.create(params[:tea][:type])
+      @tea.types << @type
       @tea.save
       flash[:message] = "New Tea added to crate!"
+      # binding.pry
       redirect "/users/#{@user.slug}/teas/#{@tea.id}"
     end
   end
 
   get '/users/:slug/teas/:id' do
-    if !logged_in?
-      redirect '/login'
-    else
+    if logged_in?
       @user = current_user
       @tea = Tea.find(params[:id])
+      # binding.pry
+      # @type = Type.find(params[:id])
       erb :'/teas/show_tea'
+    else
+      redirect '/login'
     end
   end
 
   get '/users/:slug/teas/:id/edit' do
-    if !logged_in?
-      redirect '/login'
-    else
+    if logged_in?
       @user = current_user
       @tea = Tea.find(params[:id])
       if @tea.user_id == session[:user_id]
@@ -59,19 +60,21 @@ class TeaController < ApplicationController
       else
         erb :'/teas/index'
       end
+    else
+      redirect '/login'
     end
   end
 
   patch '/users/:slug/teas/:id' do
-    if params.has_key?("")
+    if params.has_value?("")
       flash[:message] = "Please enter all fields."
       redirect "/users/#{@user.slug}/teas/edit"
     else
       @user = current_user
       @tea = Tea.find(params[:id])
-      binding.pry
+      # binding.pry
       @tea.tea_name = params[:tea][:tea_name] #tea_name can change
-      # @tea.types = params[:tea][:type] #tea types can change
+      @tea.types = params[:tea][:type] #tea types can change
       @tea.save
       flash[:message] = "Your Tea has been updated!"
       redirect "/users/#{@user.slug}/teas/#{@tea.id}"
@@ -79,18 +82,20 @@ class TeaController < ApplicationController
   end
 
   delete '/users/:slug/teas/:id/delete' do
-    if !logged_in?
-      redirect '/login'
-    else
+    if logged_in?
       @user = current_user
       @tea = Tea.find(params[:id])
+      @type = Type.find(params[:type_id])
       if @tea.user_id == session[:user_id]
         @tea.delete
+        @type.delete
         flash[:message] = "Your Tea has been removed from your crate!"
         redirect "/users/#{@user.slug}/teas"
       else
         redirect "/users/#{@user.slug}/teas"
       end
+    else
+      redirect '/login'
     end
   end
 
