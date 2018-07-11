872069cc3aa7cff5e71e2a61ef292d2e9f2a0690
github username <github email address>
2018-07-10 23:47:46 -0700

    cleaned up file; removed comments

--- config/environment.rb
+++ config/environment.rb
@@ -23,20 +23,4 @@ configure :production do
  )
 end
 
-
-
-
 require_all 'app'
-
-
-# ENV['SINATRA_ENV'] ||= "development"
-#
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
-#
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "sqlite3",
-#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
-#
-# require_all 'app'
